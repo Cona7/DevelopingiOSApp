33434473613df05709c5299807e417de9b97433c
Cona7 <jedembalce@gmail.com>
2018-10-09 18:15:26 +0200

    added imageView to present meal pictures
    assigned imageView to stackView of ViewController class
    changing pictures on imageView throught UIImagePickerController and UITapGestureRecognizer

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var nameTextLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,5 +35,35 @@ extension ViewController: UITextFieldDelegate {
 
     func textFieldDidEndEditing(_ textField: UITextField) {
         nameTextLabel.text = nameTextField.text
+    }
+}
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBAction func selectPhotoFromLibrary(_ sender: UITapGestureRecognizer) {
+        nameTextField.resignFirstResponder()
+
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self
+
+        present(imagePickerController, animated: true, completion: nil)
+
+    }
+
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+
+        guard let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+
+        photoImageView.image = selectedImage
+
+        dismiss(animated: true, completion: nil)
     }
 }
