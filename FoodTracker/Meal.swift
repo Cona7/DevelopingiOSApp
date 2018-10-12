90c2de5db565b24c1d9d4962b68cbb15cfaecd1b
Cona7 <jedembalce@gmail.com>
2018-10-12 14:18:10 +0200

    added data model Meal class and Unit Tests

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,30 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Josip Glasovac on 12/10/2018.
+//  Copyright © 2018 Josip Glasovac. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+
+    init?(name: String, photo: UIImage?, rating: Int) {
+
+        guard !name.isEmpty else {
+            return nil
+        }
+
+        guard rating >= 0 && rating <= 5 else {
+            return nil
+        }
+
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+    }
+}
---
+++ FoodTrackerTests/FoodTrackerTests.swift
@@ -0,0 +1,36 @@
+//
+//  FoodTrackerTests.swift
+//  FoodTrackerTests
+//
+//  Created by Josip Glasovac on 12/10/2018.
+//  Copyright © 2018 Josip Glasovac. All rights reserved.
+//
+
+import XCTest
+@testable import FoodTracker
+
+class FoodTrackerTests: XCTestCase {
+
+    func testMealInitializationSucceess() {
+
+        let zeroRatingMeal = Meal.init(name: "Zero", photo: nil, rating: 0)
+        XCTAssertNotNil(zeroRatingMeal)
+
+        // Highest positive rating
+        let positiveRatingMeal = Meal.init(name: "Positive", photo: nil, rating: 5)
+        XCTAssertNotNil(positiveRatingMeal)
+    }
+
+    func testMealInitializatioeFail() {
+
+        let negativeRatingMeal = Meal.init(name: "Negative", photo: nil, rating: -1)
+        XCTAssertNil(negativeRatingMeal)
+
+        let emptyStringMeal = Meal.init(name: "", photo: nil, rating: 0)
+        XCTAssertNil(emptyStringMeal)
+
+        let largeRatingMeal = Meal.init(name: "Large", photo: nil, rating: 6)
+        XCTAssertNil(largeRatingMeal)
+    }
+
+}
